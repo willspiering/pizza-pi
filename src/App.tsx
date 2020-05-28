69737c2d62f48cfe53e54f6a178bba7764be067e
Spiering <wspiering@skylinetechnologies.com>
2020-05-28 15:38:56 -0700

    pulled in files from codesandbox version

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,65 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import * as React from "react";
+import FormSection from "./components/FormSection";
+import Calculator from "./components/Calculator";
+import ListSection from "./components/ListSection";
+import useViewport from "./hooks/useViewport";
+import usePersistedState from "./hooks/usePersistedState";
+import styled from "styled-components";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface PizzaBuildState {
+	id: string;
+	size: number;
+	unitPrice: number;
+	numberOfPizzas: number;
+	slices: number;
+	areaOfPizza: number;
+	totalArea: number;
+	pricePerSqIn: number;
+	pricePerSlice: number;
+	totalPrice: number;
 }
 
-export default App;
+export default function App() {
+	const [showList, setShowList] = React.useState(false);
+	const [pizzaList, setPizzaList] = usePersistedState<PizzaBuildState[]>([], "PizzaList");
+	console.log("PizzaList: ", pizzaList);
+	const width = useViewport();
+	// TODO: setup good mobile breakpoint
+	const mobile = 600;
+
+	let addPizzaBuild = (newPizzaBuild) => {
+		setPizzaList((pizzaList) => pizzaList.concat(newPizzaBuild));
+	};
+
+	let removePizzaBuild = (id) => {
+		setPizzaList((pizzaList) => pizzaList.filter((t) => t.id !== id));
+	};
+
+	let toggleList = () => setShowList(!showList);
+
+	return (
+		<StyledApp>
+			{(!showList || width > mobile) && (
+				<FormSection>
+					<Calculator updateList={addPizzaBuild} toggleList={toggleList} />
+				</FormSection>
+			)}
+			{(showList || width > mobile) && (
+				<ListSection list={pizzaList} removePizzaBuild={removePizzaBuild} toggleList={toggleList} />
+			)}
+		</StyledApp>
+	);
+}
+
+const StyledApp = styled.div`
+	background-color: #617073ff;
+	display: flex;
+	width: 100vw;
+	height: 100vh;
+	overflow: hidden;
+	@media screen and (min-width: 600px) {
+		width: auto;
+		height: 704px;
+		border-radius: 20px;
+	}
+`;
