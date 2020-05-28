9c9707114a77535e84a94d9a04c1e9c9c24ec54a
Spiering <wspiering@skylinetechnologies.com>
2020-05-27 17:19:08 -0700

    set pizzaList to use persistedState hook

--- src/App.tsx
+++ src/App.tsx
@@ -3,66 +3,63 @@ import FormSection from "./components/FormSection";
 import Calculator from "./components/Calculator";
 import ListSection from "./components/ListSection";
 import useViewport from "./hooks/useViewport";
+import usePersistedState from "./hooks/usePersistedState";
 import styled from "styled-components";
 
 interface PizzaBuildState {
-  id: string;
-  size: number;
-  unitPrice: number;
-  numberOfPizzas: number;
-  slices: number;
-  areaOfPizza: number;
-  totalArea: number;
-  pricePerSqIn: number;
-  pricePerSlice: number;
-  totalPrice: number;
+	id: string;
+	size: number;
+	unitPrice: number;
+	numberOfPizzas: number;
+	slices: number;
+	areaOfPizza: number;
+	totalArea: number;
+	pricePerSqIn: number;
+	pricePerSlice: number;
+	totalPrice: number;
 }
 
 export default function App() {
-  const [showList, setShowList] = React.useState(false);
-  const [pizzaList, setPizzaList] = React.useState<PizzaBuildState[]>([]);
-  console.log("PizzaList: ", pizzaList);
-  const width = useViewport();
-  // TODO: setup good mobile breakpoint
-  const mobile = 600;
+	const [showList, setShowList] = React.useState(false);
+	const [pizzaList, setPizzaList] = usePersistedState([], "PizzaList");
+	console.log("PizzaList: ", pizzaList);
+	const width = useViewport();
+	// TODO: setup good mobile breakpoint
+	const mobile = 600;
 
-  let addPizzaBuild = newPizzaBuild => {
-    setPizzaList(pizzaList => pizzaList.concat(newPizzaBuild));
-  };
+	let addPizzaBuild = (newPizzaBuild) => {
+		setPizzaList((pizzaList) => pizzaList.concat(newPizzaBuild));
+	};
 
-  let removePizzaBuild = id => {
-    setPizzaList(pizzaList => pizzaList.filter(t => t.id !== id));
-  };
+	let removePizzaBuild = (id) => {
+		setPizzaList((pizzaList) => pizzaList.filter((t) => t.id !== id));
+	};
 
-  let toggleList = () => setShowList(!showList);
+	let toggleList = () => setShowList(!showList);
 
-  return (
-    <StyledApp>
-      {(!showList || width > mobile) && (
-        <FormSection>
-          <Calculator updateList={addPizzaBuild} toggleList={toggleList} />
-        </FormSection>
-      )}
-      {(showList || width > mobile) && (
-        <ListSection
-          list={pizzaList}
-          removePizzaBuild={removePizzaBuild}
-          toggleList={toggleList}
-        />
-      )}
-    </StyledApp>
-  );
+	return (
+		<StyledApp>
+			{(!showList || width > mobile) && (
+				<FormSection>
+					<Calculator updateList={addPizzaBuild} toggleList={toggleList} />
+				</FormSection>
+			)}
+			{(showList || width > mobile) && (
+				<ListSection list={pizzaList} removePizzaBuild={removePizzaBuild} toggleList={toggleList} />
+			)}
+		</StyledApp>
+	);
 }
 
 const StyledApp = styled.div`
-  background-color: #617073ff;
-  display: flex;
-  width: 100vw;
-  height: 100vh;
-  overflow: hidden;
-  @media screen and (min-width: 600px) {
-    width: auto;
-    height: 704px;
-    border-radius: 20px;
-  }
+	background-color: #617073ff;
+	display: flex;
+	width: 100vw;
+	height: 100vh;
+	overflow: hidden;
+	@media screen and (min-width: 600px) {
+		width: auto;
+		height: 704px;
+		border-radius: 20px;
+	}
 `;
